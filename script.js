f707c557e508332ff9aa2a1f4ec84d9a94fc8308
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-01-01 20:12:32 -0500

    more messages

--- script.js
+++ script.js
@@ -3,13 +3,46 @@ var text = document.getElementById("text");
 
 var messages = [
   "Take A Break",
-  "eH HEm",
-  "Trust me I just wanted fudge :)",
-  "Roses are red, violets are blue, ",
-  "HAPPY NEW YEAR!!!!"
+  "eHh HeM",
+  "Why u stilll here?",
+  "trust me I just wanted fudge :)",
+  "70UCH GR455",
+  "HAPPY NEW YEAR!!!!",
+  "Dont U Wait",
+  "B-L-A-N-K",
+  "ain't it gnarly?",
+  "want a song rec?",
+  "FUDGE COLORED SITE YESS",
+  "new year, old u",
+  "well hello there friend",
+  "404 FUN NOT FOUND",
+  "what even is life?",
+  "+ P_E_A_K + ",
+  "><>*<><",
+  "~",
+  "is tis a trap?",
+  "6-7",
+  "u da typ dat likes schooool",
+  "does this button love you back?",
+  "spaghetti has feelings",
+  "my cat owns you",
+  "play chess",
+  "u done yet?",
+  "*|*|*|*",
+  "HELPLESS",
+  "wash ur own dishes",
+  "the fridge is judging you",
+  "we dont talk bout u",
+  "friends? nah",
+  "hehehe",
+  "ain't I a genius?",
+  "D+RAM+A",
+  "IHATEJAVASCRIPT",
+  "go .",
+
 ];
 
 button.addEventListener("click", function () {
-  var randomIndex = Math.floor(Math.random() * messages.length);
-  text.textContent = messages[randomIndex];
+  var randex = Math.floor(Math.random() * messages.length);
+  text.textContent = messages[randex];
 });
