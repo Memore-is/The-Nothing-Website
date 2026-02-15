e48832b94f88a85e9a97c8283fea08ada0ffbf27
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-02-15 09:20:12 -0500

    joke api

--- corruptedscript.js
+++ corruptedscript.js
@@ -1,3 +1,14 @@
-document.getElementById("giftbox") = function () {
-    
+document.getElementById("giftbox").onclick = function () {
+    window.location.href = "broken.html";
+
+document.getElementById("button").onclick = function () {
+    fetch("https://icanhazdadjoke.com/", {
+        headers: {"Accept": "text/plain"}
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("text").textContent = data
+    })
+    .catch(e => console.log(e));
+    };
 }
